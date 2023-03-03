7a5dd94fd98533dbbd78e9331b2ad4f9a2d7f6ae
imgustacabral <imgustacabral@gmail.com>
2023-03-03 16:19:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DESAFIO DO CamelCase
+
+### Voce irá desenvolver a função camelCase()
+#### Essa função deverá receber uma frase e transforma-la em camel case.
+#### Ex: camelCase("meu texto") ==> meuTexto
+#### Use console.log() para rodar os testes, passando nos 4 testes seu código está ok!
+#### Se o tester der Fail, o próprio teste lhe mostrará o retorno que está tendo.
---
+++ index.js
@@ -0,0 +1,13 @@
+
+// Desenvolva aqui a sua lógica
+function camelCase(string) {
+  
+}
+
+
+// Testes, não precisa comentar, apenas rodar seu código e saberá o retorno!
+
+console.log(camelCase("camel case") == "camelCase" ? '\033[32mPASS - camelCase \033' : '\033[31mFAIL - ' +  camelCase("camel case") + "\033" )
+console.log(camelCase("pastel muito bom") == "pastelMuitoBom" ? '\033[32mPASS - pastelMuitoBom \033' : '\033[31mFAIL - ' +  camelCase("pastel muito bom") + "\033" )
+console.log(camelCase("testando uma frase maior") == "testandoUmaFraseMaior" ? '\033[32mPASS - testandoUmaFraseMaior \033' : '\033[31mFAIL - ' +  camelCase("testando uma frase maior") + "\033" )
+console.log(camelCase("CamelCase") == "camelCase" ? '\033[32mPASS - camelCase \033' : '\033[31mFAIL - ' +  camelCase("CamelCase") + "\033" )
